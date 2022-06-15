172517ff45d78ce177d991ef974f8ad861f8dc87
BOMEN <bomen21x@gmail.com>
2022-06-15 15:01:41 +0700

    Start Module
    View Password at Login and Signup Page

---
+++ ctp_show_password/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    Cybernetics Plus Co., Ltd.
+#    View Password at Login and Signup Page
+#
+###################################################################################
+
+{
+    'name': 'Cybernetics Plus View Password',
+    'version': '15.0.1.0.1',
+    'summary': """ 
+            View Password at Login and Signup Page
+            .""",
+    'description': """ 
+            View Password at Login and Signup Page
+            .""",
+    'author': 'Cybernetics Plus Co., Ltd.',
+    'website': 'https://www.cybernetics.plus',
+    'live_test_url': 'https://www.cybernetics.plus',
+    'images': ['static/description/banner.png'],
+    'category': 'Website',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'contributors': [
+        'Developer <dev@cybernetics.plus>',
+    ],
+  "data": ['views/auth_signup_login.xml'],
+  'assets': {
+      'web.assets_frontend': [
+          'ctp_show_password/static/src/css/show_password.css',
+          'ctp_show_password/static/src/js/show_password.js',
+      ],
+  },
+  "pre_init_hook": "pre_init_check",
+}
